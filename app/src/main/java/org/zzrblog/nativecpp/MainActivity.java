b0351f9500ab7d1781c49fd0c4fdb52327b50464
MrZhaozhirong <2377871036@qq.com>
2019-03-12 17:58:06 +0800

    Initial commit

---
+++ app/src/main/java/org/zzrblog/nativecpp/MainActivity.java
@@ -0,0 +1,9 @@
+package org.zzrblog.nativecpp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zzr on 2019/3/12.
+ */
+public class MainActivity extends AppCompatActivity {
+}
