9745d7697842af6c2f94b46acda3b8353bae8898
MrZhaozhirong <2377871036@qq.com>
2019-03-12 18:09:00 +0800

    Init

--- app/src/main/java/org/zzrblog/nativecpp/MainActivity.java
+++ app/src/main/java/org/zzrblog/nativecpp/MainActivity.java
@@ -1,9 +1,17 @@
 package org.zzrblog.nativecpp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by zzr on 2019/3/12.
  */
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
