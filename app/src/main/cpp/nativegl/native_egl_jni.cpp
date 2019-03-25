b65f90bd74af26741de481a49fb22c2226d7a2df
MrZhaozhirong <2377871036@qq.com>
2019-03-25 18:03:08 +0800

    增加libzip，指定jniLibs目录加载。

--- app/src/main/cpp/nativegl/native_egl_jni.cpp
+++ app/src/main/cpp/nativegl/native_egl_jni.cpp
@@ -4,6 +4,7 @@
 #include "../egl/GLThread.h"
 #include "NativeGLRender.h"
 #include "../common/zzr_common.h"
+#include "../include/zip.h"
 
 //////// 声明全局变量，在zzr_common.c的JNI_OnLoad定义
 extern JavaVM * gJavaVM;
@@ -11,24 +12,55 @@ extern JavaVM * gJavaVM;
 GLThread *glThread = NULL;
 NativeGLRender* testRender = NULL;
 
+extern "C"
+JNIEXPORT void JNICALL
+Java_org_zzrblog_nativecpp_NativeEGL_initBeforeEGL(JNIEnv *env, jobject instance, jobject context)
+{
+    jclass j_native_egl_class = env->GetObjectClass(instance);
+    jmethodID getResAbsolutePath_mid = env->GetMethodID(j_native_egl_class, "getPackageResourceAPK",
+                                                        "()Ljava/lang/String;");
+    jstring compressed_apk_path_jstr = static_cast<jstring>(env->CallObjectMethod(instance, getResAbsolutePath_mid));
+    const char *compressed_apk_path_cstr = env->GetStringUTFChars(compressed_apk_path_jstr, GL_FALSE);
+    LOGD("资源压缩apk文件：%s", compressed_apk_path_cstr);
 
+    jmethodID getResourceCacheDir_mid = env->GetMethodID(j_native_egl_class, "getResourceCacheDir",
+                                                        "()Ljava/lang/String;");
+    jstring release_res_path_jstr = static_cast<jstring>(env->CallObjectMethod(instance, getResourceCacheDir_mid));
+    const char *release_res_path_cstr = env->GetStringUTFChars(release_res_path_jstr, GL_FALSE);
+    LOGD("本地解压缩路径：%s", release_res_path_cstr);
+
+
+    int iErr = 0;
+    struct zip* apkArchive = zip_open(compressed_apk_path_cstr, ZIP_CHECKCONS, &iErr);
+    if(!apkArchive) {
+        LOGE("zip open failed:%d\n", iErr);
+        env->ReleaseStringUTFChars(compressed_apk_path_jstr, compressed_apk_path_cstr);
+        env->ReleaseStringUTFChars(release_res_path_jstr, release_res_path_cstr);
+        return;
+    }
+    struct zip_stat fstat;
+    zip_stat_init(&fstat);
+
+    int numFiles = zip_get_num_files(apkArchive);
+    LOGI("File numFiles %i \n",numFiles);
+    for (int i=0; i<numFiles; i++) {
+        const char* name = zip_get_name(apkArchive, i, 0);
+        if (name == NULL) {
+            LOGE("Error reading zip file name at index %i : %s", i, zip_strerror(apkArchive));
+            return;
+        }
+        zip_stat(apkArchive,name,0,&fstat);
+        LOGI("Index %i:%s      Uncompressed Size:%d    Compressed Size:%d", i, fstat.name, fstat.size, fstat.comp_size);
+
+    }
+    env->ReleaseStringUTFChars(compressed_apk_path_jstr, compressed_apk_path_cstr);
+    env->ReleaseStringUTFChars(release_res_path_jstr, release_res_path_cstr);
+}
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_org_zzrblog_nativecpp_NativeEGL_onSurfaceCreate(JNIEnv *env, jobject instance, jobject surface)
 {
-    jclass j_native_egl_class = env->GetObjectClass(instance);
-    jmethodID getResAbsolutePath_mid = env->GetMethodID(j_native_egl_class, "getPackageResourceAPK",
-                                                        "()Ljava/lang/String;");
-    jstring res_path_jstr = static_cast<jstring>(env->CallObjectMethod(instance, getResAbsolutePath_mid,
-                                                 env->NewStringUTF("/assets/mipmap/animation_test.png")));
-    const char *res_path_cstr = env->GetStringUTFChars(res_path_jstr, GL_FALSE);
-    LOGD("资源压缩文件：%s", res_path_cstr);
-    env->ReleaseStringUTFChars(res_path_jstr, res_path_cstr);
-
-
-
-
     ANativeWindow* nativeWindow = ANativeWindow_fromSurface(env, surface);
     testRender = new NativeGLRender();
     glThread = new GLThread();
--- app/src/main/java/org/zzrblog/nativecpp/NativeEGL.java
+++ app/src/main/java/org/zzrblog/nativecpp/NativeEGL.java
@@ -3,18 +3,22 @@ package org.zzrblog.nativecpp;
 import android.content.Context;
 import android.view.Surface;
 
+import java.io.File;
+
 /**
  * Created by zzr on 2019/3/14.
  */
 public class NativeEGL {
 
     static {
+        System.loadLibrary("zip");
         System.loadLibrary("native-egl");
     }
 
     private Context ctx;
     public NativeEGL(Context context) {
         ctx = context;
+        initBeforeEGL(ctx);
     }
 
     public String getPackageResourceAPK() {
@@ -22,6 +26,15 @@ public class NativeEGL {
         // /data/app/org.zzrblog.nativecpp-zzIu0MPPws9Df9SN-U1BRA==/base.apk
         // 类似这种以根目录开头的，apk后缀的压缩文件，然后再NDK层用zip库解压缩。
     }
+    public String getResourceCacheDir() {
+        File externalCacheDir = ctx.getExternalCacheDir();
+        assert externalCacheDir != null;
+        return externalCacheDir.getAbsolutePath();
+    }
+
+    public native void initBeforeEGL(Context context);
+
+
 
     public native void onSurfaceCreate(Surface surface);
 
