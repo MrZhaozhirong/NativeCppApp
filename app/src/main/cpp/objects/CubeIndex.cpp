1a931d9d027efb1d7ee2a4b7ad3a9a197b8ed40a
MrZhaozhirong <2377871036@qq.com>
2019-03-28 18:08:23 +0800

    调整坐标点和纹理坐标

--- app/src/main/cpp/objects/CubeIndex.cpp
+++ app/src/main/cpp/objects/CubeIndex.cpp
@@ -13,16 +13,22 @@ CubeIndex::CubeIndex() {
     CELLMath::Matrix::setIdentityM(modelMatrix, 0);
 
 
-    CUBE_VERTEX_DATA = new int8_t[40];
+    CUBE_VERTEX_DATA = new int8_t[60];
     int8_t * p = CUBE_VERTEX_DATA;
     p[0]=-1;   p[1]=1;    p[2]=1;    p[3]=0;   p[4]=0;
-    p[5]=1;    p[6]=1;    p[7]= 1;   p[8]=0;   p[9]=1;
-    p[10]=-1;  p[11]=-1;  p[12]= 1;  p[13]=1;  p[14]=0;
+    p[5]=1;    p[6]=1;    p[7]= 1;   p[8]=1;   p[9]=0;
+    p[10]=-1;  p[11]=-1;  p[12]= 1;  p[13]=0;  p[14]=1;
     p[15]=1;   p[16]=-1;  p[17]= 1;  p[18]=1;  p[19]=1;
     p[20]=-1;  p[21]= 1;  p[22]=-1;  p[23]=1;  p[24]=0;
     p[25]=1;   p[26]=1;   p[27]=-1;  p[28]=0;  p[29]=0;
     p[30]=-1;  p[31]=-1;  p[32]=-1;  p[33]=1;  p[34]=1;
-    p[35]=1;   p[36]=-1;  p[37]=-1;  p[38]=1;  p[39]=0;
+    p[35]=1;   p[36]=-1;  p[37]=-1;  p[38]=0;  p[39]=1;
+
+    p[40]=-1;  p[41]= 1;  p[42]=-1;  p[43]=0;  p[44]=0;
+    p[45]=1;   p[46]=1;   p[47]=-1;  p[48]=1;  p[49]=0;
+    p[50]=-1;  p[51]=1;   p[52]=1;   p[53]=0;  p[54]=1;
+    p[55]=1;   p[56]=1;   p[57]= 1;  p[58]=1;  p[59]=1;
+
     //{
     //        //x,   y,  z    s, t,
     //        -1,   1,   1,   0, 0,  // 0 left top near
@@ -33,19 +39,29 @@ CubeIndex::CubeIndex() {
     //        1,   1,  -1,    0, 0,  // 5 right top far
     //        -1,  -1,  -1,   1, 1,  // 6 left bottom far
     //        1,  -1,  -1,    1, 0,  // 7 right bottom far
+    //        这样安排的纹理坐标点，四周是正常的，但是顶底是不正常，
+    //        所以顶底要重新安排一组
+    //        -1,   1,  -1,   0, 0,  // 8  left top far
+    //        1,   1,  -1,    1, 0,  // 9  right top far
+    //        -1,   1,   1,   0, 1,  // 10 left top near
+    //        1,   1,   1,    1, 1,  // 11 right top near
     //};
 
 
 
 
     CUBE_INDEX = new int8_t[24];
-    CUBE_INDEX[0]=0;  CUBE_INDEX[1]=1;  CUBE_INDEX[2]=2;  CUBE_INDEX[3]=3;
-    CUBE_INDEX[4]=4;  CUBE_INDEX[5]=5;  CUBE_INDEX[6]=6;  CUBE_INDEX[7]=7;
-    CUBE_INDEX[8]=4;  CUBE_INDEX[9]=0;  CUBE_INDEX[10]=6; CUBE_INDEX[11]=2;
-    CUBE_INDEX[12]=1; CUBE_INDEX[13]=5; CUBE_INDEX[14]=3; CUBE_INDEX[15]=7;
-    CUBE_INDEX[16]=4; CUBE_INDEX[17]=5; CUBE_INDEX[18]=0; CUBE_INDEX[19]=1;
-    CUBE_INDEX[20]=6; CUBE_INDEX[21]=7; CUBE_INDEX[22]=2; CUBE_INDEX[23]=3;
+    CUBE_INDEX[0 ]= 8;  CUBE_INDEX[1 ]= 9;  CUBE_INDEX[2 ]=10;  CUBE_INDEX[3 ]=11;
+    CUBE_INDEX[4 ]= 6;  CUBE_INDEX[5 ]= 7;  CUBE_INDEX[6 ]=2;   CUBE_INDEX[7 ]=3;
+    CUBE_INDEX[8 ]= 0;  CUBE_INDEX[9 ]= 1;  CUBE_INDEX[10]=2;   CUBE_INDEX[11]=3;
+    CUBE_INDEX[12]= 4;  CUBE_INDEX[13]= 5;  CUBE_INDEX[14]=6;   CUBE_INDEX[15]=7;
+    CUBE_INDEX[16]= 4;  CUBE_INDEX[17]= 0;  CUBE_INDEX[18]=6;   CUBE_INDEX[19]=2;
+    CUBE_INDEX[20]= 1;  CUBE_INDEX[21]= 5;  CUBE_INDEX[22]=3;   CUBE_INDEX[23]=7;
     //{
+    //    //top
+    //    8,9,10,11,
+    //    //bottom
+    //    6,7,2,3
     //    //front
     //    0,1,2,3,
     //    //back
@@ -54,10 +70,6 @@ CubeIndex::CubeIndex() {
     //    4,0,6,2,
     //    //right
     //    1,5,3,7,
-    //    //top
-    //    4,5,0,1,
-    //    //bottom
-    //    6,7,2,3
     //};
 }
 
