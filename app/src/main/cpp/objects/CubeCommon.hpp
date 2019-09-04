bca0ab700162a9621a9061c0c308f9df3352d0cc
zzr <2377871036@qq.com>
2019-09-05 00:35:48 +0800

    普通正方体

--- app/src/main/cpp/objects/CubeCommon.hpp
+++ app/src/main/cpp/objects/CubeCommon.hpp
@@ -9,23 +9,24 @@
 #include <string.h>
 #include "../common/CELLMath.hpp"
 #include "../program/CubeComProgram.hpp"
+#include "../common/Camera3D.hpp"
 
 class CubeCommon {
 private:
-    struct V3N3T2 {
+    struct V3T2 {
         float x, y, z; //位置坐标
         float u,v; //纹理坐标
     };
 public:
     CubeComProgram          sprogram;
     GLuint                  mCubeSurfaceTexId;
-    V3N3T2                  _data[36];
+    V3T2                    _data[36];
     CELL::matrix4           mModelMatrix;
 
 
     void        init(const CELL::float3 &halfSize, GLuint tex)
     {
-        V3N3T2 verts[] =
+        V3T2 verts[] =
         {
                 // 前
                 {-halfSize.x, +halfSize.y, +halfSize.z,   0.0f,0.0f},
@@ -73,6 +74,31 @@ public:
         memcpy(_data, verts, sizeof(verts));
         mModelMatrix.identify();
         mCubeSurfaceTexId = tex;
+    }
+
+
+    void        render(Camera3D& camera)
+    {
+        sprogram.begin();
+        CELL::matrix4   model   =   mModelMatrix;
+        CELL::matrix4   vp = camera.getProject() * camera.getView();
+        CELL::matrix4   mvp = (vp * model);
+        glUniformMatrix4fv(sprogram._mvp, 1, GL_FALSE, mvp.data());
+
+        //glActiveTexture(GL_TEXTURE0);
+        //glEnable(GL_TEXTURE_2D);
+        //glBindTexture(GL_TEXTURE_2D,  mCubeSurfaceTexId);
+        //glUniform1i(sprogram._texture, 0);
+
+        glUniform3f(sprogram._lightColor, 1.0f, 0.0f, 0.0f);
+
+        glVertexAttribPointer(static_cast<GLuint>(sprogram._position), 3, GL_FLOAT, GL_FALSE,
+                              sizeof(CubeCommon::V3T2), &_data[0].x);
+        glVertexAttribPointer(static_cast<GLuint>(sprogram._uv),       2, GL_FLOAT, GL_FALSE,
+                              sizeof(CubeCommon::V3T2), &_data[0].u);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        sprogram.end();
     }
 };
 #endif //CUBE_HPP
