adc0613d69153825bdf6a97678f9a9f4faed53e7
lt_zzr <1732301994@qq.com>
2019-09-27 18:06:18 +0800

    阴影实现3

---
+++ app/src/main/cpp/objects/CubeShadow.hpp
@@ -0,0 +1,123 @@
+//
+// Created by nicky on 2019/9/27.
+//
+
+#ifndef NATIVECPPAPP_CUBESHADOW_HPP
+#define NATIVECPPAPP_CUBESHADOW_HPP
+
+#include "../program/IlluminateWithShadow.hpp"
+#include "../common/CELLMath.hpp"
+#include "../common/Camera3D.hpp"
+
+class CubeShadow {
+public:
+    struct V3N3T2 {
+        float x, y, z; //位置坐标
+        float nx, ny, nz; //法向量
+        float u,v; //纹理坐标
+    };
+
+public:
+    IlluminateWithShadow    sprogram;
+    GLuint                  mCubeSurfaceTexId;
+    GLuint                  mShadowMapId;
+    V3N3T2                  _data[36];
+    CELL::matrix4           mModelMatrix;
+
+    void        setShadowMap(GLuint texId) {
+        mShadowMapId = texId;
+    }
+    void        setSurfaceTexture(GLuint texId) {
+        mCubeSurfaceTexId = texId;
+    }
+
+    void        init(const CELL::float3 &halfSize)
+    {
+        V3N3T2 verts[] =
+        {
+                // 前
+                {-halfSize.x, +halfSize.y, +halfSize.z, 0.0f,  0.0f,  +1.0f, 0.0f,0.0f},
+                {-halfSize.x, -halfSize.y, +halfSize.z, 0.0f,  0.0f,  +1.0f, 1.0f,0.0f},
+                {+halfSize.x, +halfSize.y, +halfSize.z, 0.0f,  0.0f,  +1.0f, 0.0f,1.0f},
+                {-halfSize.x, -halfSize.y, +halfSize.z, 0.0f,  0.0f,  +1.0f, 1.0f,0.0f},
+                {+halfSize.x, -halfSize.y, +halfSize.z, 0.0f,  0.0f,  +1.0f, 1.0f,1.0f},
+                {+halfSize.x, +halfSize.y, +halfSize.z, 0.0f,  0.0f,  +1.0f, 0.0f,1.0f},
+                // 后
+                {+halfSize.x, -halfSize.y, -halfSize.z, 0.0f,  0.0f,  -1.0f, 1.0f,0.0f},
+                {-halfSize.x, -halfSize.y, -halfSize.z, 0.0f,  0.0f,  -1.0f, 1.0f,1.0f},
+                {+halfSize.x, +halfSize.y, -halfSize.z, 0.0f,  0.0f,  -1.0f, 0.0f,0.0f},
+                {-halfSize.x, +halfSize.y, -halfSize.z, 0.0f,  0.0f,  -1.0f, 1.0f,0.0f},
+                {+halfSize.x, +halfSize.y, -halfSize.z, 0.0f,  0.0f,  -1.0f, 0.0f,0.0f},
+                {-halfSize.x, -halfSize.y, -halfSize.z, 0.0f,  0.0f,  -1.0f, 1.0f,1.0f},
+                // 左
+                {-halfSize.x, +halfSize.y, +halfSize.z, -1.0f, 0.0f,  0.0f,  0.0f,0.0f},
+                {-halfSize.x, -halfSize.y, -halfSize.z, -1.0f, 0.0f,  0.0f,  1.0f,1.0f},
+                {-halfSize.x, -halfSize.y, +halfSize.z, -1.0f, 0.0f,  0.0f,  1.0f,0.0f},
+                {-halfSize.x, +halfSize.y, -halfSize.z, -1.0f, 0.0f,  0.0f,  0.0f,1.0f},
+                {-halfSize.x, -halfSize.y, -halfSize.z, -1.0f, 0.0f,  0.0f,  1.0f,1.0f},
+                {-halfSize.x, +halfSize.y, +halfSize.z, -1.0f, 0.0f,  0.0f,  0.0f,0.0f},
+                // 右
+                {+halfSize.x, +halfSize.y, -halfSize.z, +1.0f, 0.0f,  0.0f,  0.0f,0.0f},
+                {+halfSize.x, +halfSize.y, +halfSize.z, +1.0f, 0.0f,  0.0f,  0.0f,1.0f},
+                {+halfSize.x, -halfSize.y, +halfSize.z, +1.0f, 0.0f,  0.0f,  1.0f,1.0f},
+                {+halfSize.x, -halfSize.y, -halfSize.z, +1.0f, 0.0f,  0.0f,  1.0f,0.0f},
+                {+halfSize.x, +halfSize.y, -halfSize.z, +1.0f, 0.0f,  0.0f,  0.0f,0.0f},
+                {+halfSize.x, -halfSize.y, +halfSize.z, +1.0f, 0.0f,  0.0f,  1.0f,1.0f},
+                // 上
+                {-halfSize.x, +halfSize.y, +halfSize.z, 0.0f,  +1.0f, 0.0f,  0.0f,1.0f},
+                {+halfSize.x, +halfSize.y, +halfSize.z, 0.0f,  +1.0f, 0.0f,  1.0f,1.0f},
+                {+halfSize.x, +halfSize.y, -halfSize.z, 0.0f,  +1.0f, 0.0f,  1.0f,0.0f},
+                {-halfSize.x, +halfSize.y, -halfSize.z, 0.0f,  +1.0f, 0.0f,  0.0f,0.0f},
+                {-halfSize.x, +halfSize.y, +halfSize.z, 0.0f,  +1.0f, 0.0f,  0.0f,1.0f},
+                {+halfSize.x, +halfSize.y, -halfSize.z, 0.0f,  +1.0f, 0.0f,  1.0f,0.0f},
+                // 下
+                {+halfSize.x, -halfSize.y, -halfSize.z, 0.0f,  -1.0f, 0.0f,  1.0f,1.0f},
+                {+halfSize.x, -halfSize.y, +halfSize.z, 0.0f,  -1.0f, 0.0f,  1.0f,0.0f},
+                {-halfSize.x, -halfSize.y, -halfSize.z, 0.0f,  -1.0f, 0.0f,  0.0f,1.0f},
+                {+halfSize.x, -halfSize.y, +halfSize.z, 0.0f,  -1.0f, 0.0f,  1.0f,0.0f},
+                {-halfSize.x, -halfSize.y, +halfSize.z, 0.0f,  -1.0f, 0.0f,  0.0f,0.0f},
+                {-halfSize.x, -halfSize.y, -halfSize.z, 0.0f,  -1.0f, 0.0f,  0.0f,1.0f}
+        };
+        memcpy(_data, verts, sizeof(verts));
+        mModelMatrix.identify();
+
+        sprogram.initialize();
+    }
+
+    void        render(Camera3D& camera, real3& lightPos, matrix4 lightSpaceMatrix)
+    {
+        sprogram.begin();
+        //CELL::matrix4   model   =   mModelMatrix;
+        //CELL::matrix4   vp = camera.getProject() * camera.getView();
+        //CELL::matrix4   mvp = (vp * model);
+        glUniformMatrix4fv(sprogram._projection, 1, GL_FALSE,
+                           reinterpret_cast<const GLfloat *>(camera.getProject().data()));
+        glUniformMatrix4fv(sprogram._view, 1, GL_FALSE,
+                           reinterpret_cast<const GLfloat *>(camera.getView().data()));
+        glUniformMatrix4fv(sprogram._model, 1, GL_FALSE, mModelMatrix.data());
+
+        glActiveTexture(GL_TEXTURE0);
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D,  mCubeSurfaceTexId);
+        glUniform1i(sprogram._texture, 0);
+
+        glActiveTexture(GL_TEXTURE1);
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D,  mShadowMapId);
+        glUniform1i(sprogram._shadowMap, 1);
+
+        glUniform3f(sprogram._lightColor, 1.0f, 1.0f, 1.0f);
+        glUniform3f(sprogram._lightPos, lightPos.x, lightPos.y, lightPos.z);
+        glUniformMatrix4fv(sprogram._lightSpaceMatrix, 1, GL_FALSE, lightSpaceMatrix.data());
+
+        glVertexAttribPointer(static_cast<GLuint>(sprogram._position), 3, GL_FLOAT, GL_FALSE,
+                              sizeof(CubeShadow::V3N3T2), &_data[0].x);
+        glVertexAttribPointer(static_cast<GLuint>(sprogram._normal),   3, GL_FLOAT, GL_FALSE,
+                              sizeof(CubeShadow::V3N3T2), &_data[0].nx);
+        glVertexAttribPointer(static_cast<GLuint>(sprogram._texCoords),2, GL_FLOAT, GL_FALSE,
+                              sizeof(CubeShadow::V3N3T2), &_data[0].u);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        sprogram.end();
+    }
+};
+#endif //NATIVECPPAPP_CUBESHADOW_HPP
--- app/src/main/cpp/program/IlluminateWithShadow.hpp
+++ app/src/main/cpp/program/IlluminateWithShadow.hpp
@@ -2,100 +2,153 @@
 // Created by nicky on 2019/9/20.
 //
 
+#include "ShaderProgram.h"
+#include "ShaderHelper.h"
+
 #ifndef SHADER_ILLUMINATE_WITH_SHADOW_HPP
 #define SHADER_ILLUMINATE_WITH_SHADOW_HPP
 
+class IlluminateWithShadow : public ShaderProgram
+{
+public:
+    GLint       _position;
+    GLint       _normal;
+    GLint       _texCoords;
+
+    GLint       _projection;
+    GLint       _view;
+    GLint       _model;
+    GLint       _lightSpaceMatrix;
+
+    GLint       _lightColor;
+    GLint       _texture;
+    GLint       _shadowMap;
+    GLint       _lightPos;
+    GLint       _viewPos;// 需要镜面发射才使用
+public:
+    virtual void    initialize()
+    {
+        const char* vs  =   "#version 320 es\n"
+                            "in  vec3    position;\n"
+                            "in  vec3    normal;\n"
+                            "in  vec2    texCoords;\n"
+                            "\n"
+                            "out vec2    TexCoords;\n"
+                            "out VS_OUT {\n"
+                            "    vec3 FragPosWorldSpace;\n"
+                            "    vec3 Normal;\n"
+                            "    vec2 TexCoords;\n"
+                            "    vec4 FragPosLightSpace;\n"
+                            "} vs_out;\n"
+                            "\n"
+                            "uniform mat4 projection;\n"
+                            "uniform mat4 view;\n"
+                            "uniform mat4 model;\n"
+                            "uniform mat4 lightSpaceMatrix;\n"
+                            "\n"
+                            "void main()\n"
+                            "{\n"
+                            "    gl_Position = projection * view * model * vec4(position, 1.0f);\n"
+                            "    vs_out.TexCoords = texCoords; // 纹理uv坐标传递到fs，进行插值\n"
+                            "    vs_out.Normal = transpose(inverse(mat3(model))) * normal;\n"
+                            "    vs_out.FragPosWorldSpace = vec3(model * vec4(position, 1.0));\n"
+                            "    vs_out.FragPosLightSpace = lightSpaceMatrix * vec4(vs_out.FragPosWorldSpace, 1.0);\n"
+                            "}";
+
+        const char* fs  =   "#version 320 es\n"
+                            "uniform vec3        _lightColor;\n"
+                            "uniform sampler2D   _texture;\n"
+                            "uniform sampler2D   _shadowMap;\n"
+                            "uniform vec3        _lightPos;\n"
+                            "uniform vec3        _viewPos;\n"
+                            "\n"
+                            "in  VS_OUT {\n"
+                            "    vec3 FragPosWorldSpace;\n"
+                            "    vec3 Normal;\n"
+                            "    vec2 TexCoords;\n"
+                            "    vec4 FragPosLightSpace;\n"
+                            "} fs_in;\n"
+                            "\n"
+                            "out     vec4        fragColor;\n"
+                            "\n"
+                            "float ShadowCalculation(vec4 fragPosLightSpace)\n"
+                            "{\n"
+                            "    // 执行透视除法，剔除透视效果(2D->3D)\n"
+                            "    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;\n"
+                            "    // 上面的projCoords的xyz分量都是[-1,1]，将它变换到[0,1]的范围才好和深度图比较\n"
+                            "    projCoords = projCoords * 0.5 + 0.5;\n"
+                            "    // 取得最近点的深度(使用[0,1]范围下的fragPosLight当坐标)\n"
+                            "    float shadowDepth = texture(_shadowMap, projCoords.xy).r;\n"
+                            "    // 取得当前片元在光源视角下的深度\n"
+                            "    float currentDepth = projCoords.z;\n"
+                            "    // 检查当前片元是否在阴影中\n"
+                            "    float shadow = currentDepth > shadowDepth  ? 1.0 : 0.0;\n"
+                            "    return shadow;\n"
+                            "}\n"
+                            "void main()\n"
+                            "{\n"
+                            "    vec3 color = texture(_texture, fs_in.TexCoords).rgb;\n"
+                            "    vec3 normal = normalize(fs_in.Normal);\n"
+                            "    vec3 lightColor = _lightColor;\n"
+                            "    // 周围环境\n"
+                            "    vec3 ambient = 0.15 * color;\n"
+                            "    // 漫反射\n"
+                            "    vec3 lightDir = normalize(_lightPos - fs_in.FragPosWorldSpace);\n"
+                            "    float diff = max(dot(lightDir, normal), 0.0);\n"
+                            "    vec3 diffuse = diff * lightColor;\n"
+                            "    // 镜面发射\n"
+                            "    //#vec3 viewDir = normalize(viewPos - fs_in.FragPos);\n"
+                            "    //#vec3 reflectDir = reflect(-lightDir, normal);\n"
+                            "    //#float spec = 0.0;\n"
+                            "    //#vec3 halfwayDir = normalize(lightDir + viewDir);\n"
+                            "    //#spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);\n"
+                            "    //#vec3 specular = spec * lightColor;\n"
+                            "    // 计算阴影\n"
+                            "    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);\n"
+                            "    //vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;\n"
+                            "    vec3 lighting = (ambient + (1.0 - shadow) * diffuse) * color;\n"
+                            "    fragColor = vec4(lighting, 1.0f);\n"
+                            "}";
+
+        programId   =   ShaderHelper::buildProgram(vs, fs);
+
+        _position   =   glGetAttribLocation(programId,  "position");
+        _normal     =   glGetAttribLocation(programId,  "normal");
+        _texCoords  =   glGetAttribLocation(programId,  "texCoords");
+
+        _projection =   glGetUniformLocation(programId, "projection");
+        _view       =   glGetUniformLocation(programId, "view");
+        _model      =   glGetUniformLocation(programId, "model");
+        _lightSpaceMatrix = glGetUniformLocation(programId, "lightSpaceMatrix");
+
+        _lightColor =   glGetUniformLocation(programId, "_lightColor");
+        _texture    =   glGetUniformLocation(programId, "_texture");
+        _shadowMap  =   glGetUniformLocation(programId, "_shadowMap");
+        _lightPos   =   glGetUniformLocation(programId, "_lightPos");
+    }
+
+    /**
+    *   使用shader
+    */
+    virtual void    begin()
+    {
+        glEnableVertexAttribArray(_position);
+        glEnableVertexAttribArray(_normal);
+        glEnableVertexAttribArray(_texCoords);
+        glUseProgram(programId);
+    }
+    /**
+    *   结束
+    */
+    virtual void    end()
+    {
+        glDisableVertexAttribArray(_position);
+        glDisableVertexAttribArray(_normal);
+        glDisableVertexAttribArray(_texCoords);
+        glUseProgram(0);
+    }
+};
 #endif //SHADER_ILLUMINATE_WITH_SHADOW_HPP
 
 
-#version 320 es
-in  vec3    position;
-in  vec3    normal;
-in  vec2    texCoords;
 
-out vec2    TexCoords;
-out VS_OUT {
-    vec3 FragPosWorldSpace;
-    vec3 Normal;
-    vec2 TexCoords;
-    vec4 FragPosLightSpace;
-} vs_out;
-
-uniform mat4 projection;
-uniform mat4 view;
-uniform mat4 model;
-uniform mat4 lightSpaceMatrix;
-
-void main()
-{
-    gl_Position = projection * view * model * vec4(position, 1.0f);
-    vs_out.TexCoords = texCoords; // 纹理uv坐标传递到fs，进行插值
-    vs_out.Normal = transpose(inverse(mat3(model))) * normal;
-    vs_out.FragPosWorldSpace = vec3(model * vec4(position, 1.0));
-    vs_out.FragPosLightSpace = lightSpaceMatrix * vec4(vs_out.FragPosWorldSpace, 1.0);
-}
-
-
-
-
-
-
-
-#version 320 es
-uniform vec3        _lightColor;
-uniform sampler2D   _texture;
-uniform sampler2D   _shadowMap;
-
-uniform vec3        _lightPos;
-uniform vec3        _viewPos;
-
-in  VS_OUT {
-    vec3 FragPosWorldSpace;
-    vec3 Normal;
-    vec2 TexCoords;
-    vec4 FragPosLightSpace;
-} fs_in;
-
-out     vec4        fragColor;
-
-float ShadowCalculation(vec4 fragPosLightSpace)
-{
-    // 执行透视除法，剔除透视效果(2D->3D)
-    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
-    // 上面的projCoords的xyz分量都是[-1,1]，将它变换到[0,1]的范围才好和深度图比较
-    projCoords = projCoords * 0.5 + 0.5;
-    // 取得最近点的深度(使用[0,1]范围下的fragPosLight当坐标)
-    float shadowDepth = texture(_shadowMap, projCoords.xy).r;
-    // 取得当前片元在光源视角下的深度
-    float currentDepth = projCoords.z;
-    // 检查当前片元是否在阴影中
-    float shadow = currentDepth > shadowDepth  ? 1.0 : 0.0;
-
-    return shadow;
-}
-
-void main()
-{
-    vec3 color = texture(_texture, fs_in.TexCoords).rgb;
-    vec3 normal = normalize(fs_in.Normal);
-    vec3 lightColor = _lightColor;
-    // 周围环境
-    vec3 ambient = 0.15 * color;
-    // 漫反射
-    vec3 lightDir = normalize(_lightPos - fs_in.FragPosWorldSpace);
-    float diff = max(dot(lightDir, normal), 0.0);
-    vec3 diffuse = diff * lightColor;
-    // Specular
-    //vec3 viewDir = normalize(viewPos - fs_in.FragPos);
-    //vec3 reflectDir = reflect(-lightDir, normal);
-    //float spec = 0.0;
-    //vec3 halfwayDir = normalize(lightDir + viewDir);
-    //spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
-    //vec3 specular = spec * lightColor;
-    // 计算阴影
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);
-    //vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;
-    vec3 lighting = (ambient + (1.0 - shadow) * diffuse) * color;
-
-    fragColor = vec4(lighting, 1.0f);
-}
