b901139daab54b9e642a6b817cfaa263008a1a9c
ZhaoZhiRong <2377871036@qq.com>
2021-03-01 14:35:08 +0800

    增加FBO_MSAA

--- app/src/main/cpp/common/FrameBufferObject.hpp
+++ app/src/main/cpp/common/FrameBufferObject.hpp
@@ -8,8 +8,10 @@
 #define FBO_NONE  0x0000
 #define FBO_RGBA  0x0001
 #define FBO_DEPTH 0x0002
+#define FBO_MSAA  0x0003
 
 #include <GLES3/gl3.h>
+#include <GLES3/gl32.h>
 #include "zzr_common.h"
 
 
@@ -24,7 +26,24 @@ private:
     unsigned int   _rgbaTexId;
     unsigned int   _depthTexId;
     unsigned int   _depthRboId;
+    unsigned int   _multisampleTexId;
+    unsigned int   _multisampleRboId;
     unsigned int   _type;
+
+    void    createRgbaTexture()
+    {
+        glGenTextures(1, &_rgbaTexId);
+        //LOGE("createFBOTexture check err 1 : 0x%08x\n", glGetError());
+        glBindTexture(GL_TEXTURE_2D, _rgbaTexId);
+        //LOGE("createFBOTexture check err 2 : 0x%08x\n", glGetError());
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        //LOGE("createFBOTexture check err 3 : 0x%08x\n", glGetError());
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+        //LOGE("createFBOTexture check err 4 : 0x%08x\n", glGetError());
+    }
 
     void    createDepthTexture()
     {
@@ -51,21 +70,6 @@ private:
         //GL_DEPTH_COMPONENT -> GL_UNSIGNED_SHORT/GL_UNSIGNED_INT 才能生成depth深度位图
     }
 
-    void    createRgbaTexture()
-    {
-        glGenTextures(1, &_rgbaTexId);
-        //LOGE("createFBOTexture check err 1 : 0x%08x\n", glGetError());
-        glBindTexture(GL_TEXTURE_2D, _rgbaTexId);
-        //LOGE("createFBOTexture check err 2 : 0x%08x\n", glGetError());
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        //LOGE("createFBOTexture check err 3 : 0x%08x\n", glGetError());
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
-        //LOGE("createFBOTexture check err 4 : 0x%08x\n", glGetError());
-    }
-
     void    createDepthRenderBuffer()
     {
         glGenRenderbuffers( 1, &_depthRboId );
@@ -74,6 +78,21 @@ private:
         LOGE("glRenderbufferStorage check err : 0x%08x\n", glGetError());
     }
 
+    void createMultisampleTexture()
+    {
+        glGenTextures(1, &_multisampleTexId);
+        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, _multisampleTexId);
+        // glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGB, _width, _height, GL_TRUE);
+        glTexStorage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGBA8, _width, _height, GL_TRUE);
+        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
+    }
+
+    void createMultisampleRenderBuffer()
+    {
+        glGenRenderbuffers(1, &_multisampleRboId);
+        glBindRenderbuffer(GL_RENDERBUFFER, _multisampleRboId);
+        glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_RGBA8, _width, _height);
+    }
 public:
     FrameBufferObject()
     {
@@ -95,6 +114,7 @@ public:
     bool isInstantiation() {
         return _width!=0||_height!=0;
     }
+
 
     bool    setup(int w, int h, int type)
     {
@@ -123,6 +143,16 @@ public:
             createDepthRenderBuffer();
             glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depthRboId);
             //LOGE("after glFramebufferRenderbuffer : 0x%08x\n", glGetError());
+        }
+
+        // https://blog.csdn.net/cui6864520fei000/article/details/89949920
+        // https://www.khronos.org/opengl/wiki/GL_EXT_framebuffer_multisample
+        // https://www.xuebuyuan.com/845887.html
+        if(_type == FBO_MSAA) {
+            createMultisampleTexture();
+            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, _multisampleTexId, 0);
+            createMultisampleRenderBuffer();
+            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_RENDERBUFFER, _multisampleRboId);
         }
 
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
